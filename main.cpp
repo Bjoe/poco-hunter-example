4aea60afd64763383697bdd87d50454ea9d7ea02
Joerg Boehme <joerg@chaosdorf.de>
2019-02-22 12:17:02 +0100

    Add SHA256 example with Poco

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 
+#include "Poco/Base64Encoder.h"
+#include "Poco/Crypto/DigestEngine.h"
+
+#include <string>
+#include <sstream>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hello hunter Poco\n";
+
+  std::istringstream istr(argv[1], std::ios::binary);
+  std::string outstring;
+  std::ostringstream ostr(outstring);
+
+  Poco::Crypto::DigestEngine engine("SHA256");
+  engine.update(istr.str());
+  const Poco::DigestEngine::Digest& digest = engine.digest();
+
+  Poco::Base64Encoder encoder(ostr);
+
+  encoder << Poco::Crypto::DigestEngine::digestToHex(digest);
+
+  std::cout << ostr.str() << '\n';
   return 0;
 }
